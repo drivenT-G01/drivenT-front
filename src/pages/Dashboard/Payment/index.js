343ac232f7aa29af9c860681ff3e8f52c0c53203
samuel <shemlouk@gmail.com>
2023-05-10 11:30:52 -0300

    feat: add title to payment page

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,14 @@
+import Typography from '@material-ui/core/Typography';
+import styled from 'styled-components';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Ingresso e pagamento</StyledTypography>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;

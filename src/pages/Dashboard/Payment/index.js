984c6db1b58cc311bd80d11799cd95d1ee2fb859
samuel <shemlouk@gmail.com>
2023-05-10 17:58:43 -0300

    feat: implement ticket-context to app and payment page

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,10 +1,14 @@
 import DashboardPageContainer from '../../../layouts/DashboardPageContainer';
 import FinishPayment from './FinishPayment';
+import { useContext } from 'react';
+import TicketContext from '../../../contexts/TicketContext';
 
 export default function Payment() {
+  const { ticket, ticketLoading } = useContext(TicketContext);
+
   return (
     <DashboardPageContainer title="Ingresso e pagamento">
-      <FinishPayment />
+      {ticketLoading ? <>Loading</> : <>{ticket ? <FinishPayment {...ticket} /> : <>Em breve: ReserveTicket</>}</>}
     </DashboardPageContainer>
   );
 }

9f7aa377d1e7c079da8c0b4a394cb0fb6df7f9aa
samuel <shemlouk@gmail.com>
2023-05-10 12:07:15 -0300

    feat: create section component

---
+++ src/components/Dashboard/Section/index.js
@@ -0,0 +1,27 @@
+import { Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+export default function Section({ title = '', children }) {
+  return (
+    <Container>
+      <StyledTypography>{title}</StyledTypography>
+      <ChildrenContainer>{children}</ChildrenContainer>
+    </Container>
+  );
+}
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 10px !important;
+  font-size: 20px !important;
+  color: #8e8e8e;
+`;
+
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 44px;
+`;
+
+const ChildrenContainer = styled.div`
+  display: flex;
+`;

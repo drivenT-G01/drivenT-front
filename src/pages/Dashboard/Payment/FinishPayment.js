170d73d559033201c2ecc6d9497061a0356f8060
samuel <shemlouk@gmail.com>
2023-05-11 15:38:40 -0300

    feat: replace props with ticket-context and prevent form from appearing when ticket is paid

--- src/pages/Dashboard/Payment/FinishPayment.js
+++ src/pages/Dashboard/Payment/FinishPayment.js
@@ -1,16 +1,30 @@
 import Section from '../../../components/Dashboard/Section';
 import Card from './Card';
+import CreditCardForm from './CreditCardForm';
+import { useContext } from 'react';
+import TicketContext from '../../../contexts/TicketContext';
 
-export default function FinishPayment({ TicketType: { price, isRemote, includesHotel } }) {
-  const firstText = isRemote ? 'Online' : 'Presencial';
-  const secondText = includesHotel ? ' + Com Hotel' : ' + Sem Hotel';
-  const fullText = firstText + (isRemote ? '' : secondText);
+export default function FinishPayment() {
+  const {
+    ticket: {
+      status,
+      TicketType: { price, isRemote, includesHotel },
+    },
+  } = useContext(TicketContext);
 
   return (
     <>
       <Section title="Ingresso escolhido">
-        <Card title={fullText} description={`R$ ${price}`} stretch active></Card>
+        <Card title={createTicketReviewText(isRemote, includesHotel)} description={`R$ ${price}`} stretch active></Card>
       </Section>
+
+      <Section title="Pagamento">{status === 'PAID' ? <>Pago!</> : <CreditCardForm />}</Section>
     </>
   );
 }
+
+const createTicketReviewText = (isRemote, includesHotel) => {
+  const firstPartOfText = isRemote ? 'Online' : 'Presencial';
+  const secondPartOfText = includesHotel ? ' + Com Hotel' : ' + Sem Hotel';
+  return firstPartOfText + (isRemote ? '' : secondPartOfText);
+};

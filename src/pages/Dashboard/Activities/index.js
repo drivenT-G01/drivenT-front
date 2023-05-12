b465fa72d47f876da252f4de8439dee703dbb076
samuel <shemlouk@gmail.com>
2023-05-11 23:34:41 -0300

    feat: add overlay-message to activities page for when ticket is not paid or is online

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,5 +1,21 @@
+import { useContext } from 'react';
 import DashboardPageContainer from '../../../layouts/DashboardPageContainer';
+import TicketContext from '../../../contexts/TicketContext';
 
 export default function Activities() {
-  return <DashboardPageContainer title="Escolha de atividades"></DashboardPageContainer>;
+  const { ticket } = useContext(TicketContext);
+
+  return (
+    <DashboardPageContainer title="Escolha de atividades" message={selectMessage(ticket)}></DashboardPageContainer>
+  );
 }
+
+const selectMessage = (ticket) => {
+  const NoPaymentMessage = 'Você precisa ter confirmado pagamento antes de fazer a escolha de atividades';
+  const RemoteTicketMessage =
+    'Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades.';
+
+  if (ticket?.status !== 'PAID') return NoPaymentMessage;
+  if (ticket.TicketType.isRemote) return RemoteTicketMessage;
+  return null;
+};

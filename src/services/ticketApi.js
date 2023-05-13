b8416f1edbfcb3cd6eda9a011ff9e44efa55e4dc
samuel <shemlouk@gmail.com>
2023-05-13 19:47:53 -0300

    feat: add get-tickets-types function to ticket-api

--- src/services/ticketApi.js
+++ src/services/ticketApi.js
@@ -1,6 +1,17 @@
 import api from './api';
 
+const baseRouteUrl = '/tickets';
+
+const setAuthToken = (token) => {
+  return { headers: { Authorization: `Bearer ${token}` } };
+};
+
 export async function getUserTicket(token) {
-  const { data } = await api.get('/tickets', { headers: { Authorization: `Bearer ${token}` } });
+  const { data } = await api.get(baseRouteUrl, setAuthToken(token));
+  return data;
+}
+
+export async function getTicketTypes(token) {
+  const { data } = await api.get(`${baseRouteUrl}/types`, setAuthToken(token));
   return data;
 }

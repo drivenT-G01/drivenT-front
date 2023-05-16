153f8c9beb8191b2833324229d33a280fc1b3336
samuel <shemlouk@gmail.com>
2023-05-16 16:34:09 -0300

    feat: create oauth-button form component

---
+++ src/components/Form/OAuthButton.js
@@ -0,0 +1,27 @@
+import MuiButton from '@material-ui/core/Button';
+import styled from 'styled-components';
+
+export default function OAuthButton({ logoImage, onClickHandler = () => {}, children, ...props }) {
+  return (
+    <StyledMuiButton variant="contained" {...props} onClick={onClickHandler}>
+      <LogoImage src={logoImage} alt="logo" />
+      {children}
+    </StyledMuiButton>
+  );
+}
+
+const StyledMuiButton = styled(MuiButton)`
+  margin-top: 8px !important;
+  display: flex !important;
+  align-items: center !important;
+  justify-content: center !important;
+  padding: 8px !important;
+
+  background-color: ${({ bgcolor }) => bgcolor} !important;
+  color: ${({ color }) => color} !important;
+`;
+
+const LogoImage = styled.img`
+  height: 25px;
+  margin-right: 20px;
+`;

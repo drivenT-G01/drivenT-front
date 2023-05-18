6f16c9d0615e8f60479095625e903c8b8a30ffa9
samuel <shemlouk@gmail.com>
2023-05-17 23:07:16 -0300

    feat: add sign-in-with-git-hub function to auth-api

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -4,4 +4,8 @@ export async function signIn(email, password) {
   const response = await api.post('/auth/sign-in', { email, password });
   return response.data;
 }
-//
+
+export async function signInWithGitHub(code) {
+  const { data } = await api.post('/auth/oauth-github', { code });
+  return data;
+}

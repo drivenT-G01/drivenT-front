6674ac2c6060c59a2b5f11e75652c3af157ccdc5
Erick Mendes <erickmendesweb@gmail.com>
2023-05-18 15:02:01 -0300

    feat: booking summary page

---
+++ src/hooks/api/useHasBooking.js
@@ -0,0 +1,16 @@
+import * as bookingApiByUserId from '../../services/getBookingByUserIdApi';
+import useAsync from '../useAsync';
+import useToken from '../useToken';
+
+export default function userHasBooking() {
+  const token = useToken();
+
+  const {
+    data: userHasBooking,
+    loading: userHasBookingLoading,
+    error: userHasBookingError,
+    act: userHasBookingFunction,
+  } = useAsync((userId) => bookingApiByUserId.getBookingByUserId(token, userId), false);
+
+  return { userHasBooking, userHasBookingLoading, userHasBookingError, userHasBookingFunction };
+}

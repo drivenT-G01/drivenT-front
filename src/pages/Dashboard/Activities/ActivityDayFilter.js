9a5d665cc547c4e348a57c3edae8b90125d3cbb8
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-23 00:24:18 -0300

    feat: adding ActivityDayFilter component

---
+++ src/pages/Dashboard/Activities/ActivityDayFilter.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default function ActivityDayFilter() {
+  return (
+    <ActivityDayFilterContainer>
+
+    </ActivityDayFilterContainer>
+  );
+}
+
+const ActivityDayFilterContainer = styled.main`
+
+`;
--- src/pages/Dashboard/Activities/ChooseActivities.js
+++ src/pages/Dashboard/Activities/ChooseActivities.js
@@ -1,3 +1,22 @@
+import styled from 'styled-components';
+import ActivityDayFilter from './ActivityDayFilter';
+
 export default function ChooseActivity() {
-  return 'batata';
+  return (
+    <ChooseActivityContainer>
+      <h1>Primeiro, filtre pelo dia do evento</h1>
+      <ActivityDayFilter></ActivityDayFilter>
+    </ChooseActivityContainer>
+  );
 }
+
+const ChooseActivityContainer = styled.main`
+    display: flex;
+    h1{
+        font-family: 'Roboto', sans-serif;
+        font-weight: 400;
+        font-size: 1rem;
+        line-height: 24px;
+        color: #8E8E8E;
+    }
+`;

b54d8f134ce7b0c204a5f0b46caa823ccd62b488
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-23 20:26:55 -0300

    feat: adding date button, filtering by date

--- src/pages/Dashboard/Activities/ActivityDayCard.js
+++ src/pages/Dashboard/Activities/ActivityDayCard.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
-export default function ActivityDayCard({ activities }) {
+export default function ActivityDayCard({ date }) {
+  const options = { weekday: 'long', day: 'numeric', month: 'numeric' };
+  const formattedDate = new Date(date).toLocaleDateString('pt-BR', options);
+  const weekday = formattedDate.split(', ')[0].replace('-feira', '');
+  const numericDate = formattedDate.split(', ')[1];
+
   return (
     <ActivityDayCardContainer>
-
+      <h2>{weekday}, {numericDate}</h2>
     </ActivityDayCardContainer>
   );
 }
@@ -13,10 +18,18 @@ const ActivityDayCardContainer = styled.main`
   width: 6.5rem;
   height: 2.2rem;
   align-items: center;
+  justify-content: center;
   flex-direction: column;
   background-color: #E0E0E0;
   margin-right: 1rem;
   border-radius: 4px;
   box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
   cursor: pointer;
+  h2{
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 0.75rem;
+    line-height: 17px;
+    color: black;
+  }
 `;
--- src/pages/Dashboard/Activities/ActivityDayFilter.js
+++ src/pages/Dashboard/Activities/ActivityDayFilter.js
@@ -2,13 +2,25 @@ import styled from 'styled-components';
 import ActivityDayCard from './ActivityDayCard';
 
 export default function ActivityDayFilter({ activities }) {
-  console.log(activities);
+  const uniqueDates = new Set();
+
+  activities.forEach((activity) => {
+    const activityDate = new Date(activity.schedule.date).toDateString();
+    uniqueDates.add(activityDate);
+  });
+
+  const uniqueDatesArray = Array.from(uniqueDates).sort((a, b) => {
+    const dateA = new Date(a);
+    const dateB = new Date(b);
+    return dateA - dateB;
+  }); 
+  console.log(uniqueDatesArray);
   return (
     <ActivityDayFilterContainer>
-      {activities.map((activity) => (
+      {uniqueDatesArray.map((date) => (
         <ActivityDayCard
-          key={activity.id}
-          name={activity.name}
+          key={date}
+          date={date}
         />
       ))}
     </ActivityDayFilterContainer>

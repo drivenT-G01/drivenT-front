61262380a9524ccb90750f7e187396ed6fdfa12c
Arthur Saito <arthur_saito1998@hotmail.com>
2023-05-23 18:50:10 -0300

    feat: adding useActivity

---
+++ src/hooks/api/useActivity.js
@@ -0,0 +1,16 @@
+import * as activityApi from '../../services/getAllActivities';
+import useAsync from '../useAsync';
+import useToken from '../useToken';
+
+export default function useActivity() {
+  const token = useToken();
+
+  const {
+    data: activities,
+    loading: activitiesLoading,
+    error: activitiesError,
+    act: activitiesFunction,
+  } = useAsync(() => activityApi.getAllHotels(token));
+
+  return { activities, activitiesLoading, activitiesError, activitiesFunction };
+}

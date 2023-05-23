68d4875f89b095e5d92e2d64043bab8eb0cc3103
samuel <shemlouk@gmail.com>
2023-05-23 18:57:52 -0300

    feat: create activities-display component and add it its page

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,12 +1,15 @@
 import { useContext } from 'react';
 import DashboardPageContainer from '../../../layouts/DashboardPageContainer';
 import TicketContext from '../../../contexts/TicketContext';
+import ActivitiesDisplay from './ActivitiesDisplay';
 
 export default function Activities() {
   const { ticket } = useContext(TicketContext);
 
   return (
-    <DashboardPageContainer title="Escolha de atividades" message={selectMessage(ticket)}></DashboardPageContainer>
+    <DashboardPageContainer title="Escolha de atividades" message={selectMessage(ticket)}>
+      <ActivitiesDisplay />
+    </DashboardPageContainer>
   );
 }
 
